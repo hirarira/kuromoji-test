9912e3d4fef0a933fcfb7a64b27524a342c6f311
hirarira <hirarira617@gmail.com>
2021-05-30 19:36:25 +0900

    初期設定をClass側に持ってくる

--- test02.js
+++ test02.js
@@ -5,14 +5,23 @@ const builder = kuromoji.builder({
 });
 
 class KuromojiWords {
-  constructor(baseWords, tokenizer) {
-    const oneLines = baseWords.split('\n');
-    this.words = oneLines.reduce((base, line) => {
-      let lineWords = tokenizer.tokenize(line);
-      return base.concat(lineWords);
-    }, []);
-    // 重複を除外
-    this.words = Array.from(new Set(this.words));
+  constructor(fileName) {
+    return new Promise((resolve, reject) => {
+      builder.build(async (err, tokenizer)=>{
+        if(err) {
+          reject(err);
+        }
+        const resdAllStr = await fs.readFile(fileName, 'utf-8');
+        const oneLines = resdAllStr.split('\n');
+        this.words = oneLines.reduce((base, line) => {
+          let lineWords = tokenizer.tokenize(line);
+          return base.concat(lineWords);
+        }, []);
+        // 重複を除外
+        this.words = Array.from(new Set(this.words));
+        resolve(this);
+      })
+    })
   }
   getRandWord(num) {
     return [...Array(num)].reduce((base)=>{
@@ -22,13 +31,8 @@ class KuromojiWords {
   }
 }
 
-builder.build(async (err, tokenizer)=>{
-  if(err) {
-    console.log(err);
-    return;
-  }
+(async ()=>{
   const fileName = './sample.txt';
-  const resdAllStr = await fs.readFile(fileName, 'utf-8');
-  const kuromojiWords = new KuromojiWords(resdAllStr, tokenizer);
+  const kuromojiWords = await new KuromojiWords(fileName);
   console.log(kuromojiWords.getRandWord(20));
-})
+})();
